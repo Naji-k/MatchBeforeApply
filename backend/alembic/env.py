1bfb88432c16dadcb0279ab110ee13b0e6c9ccde
Naji Kanounji <n-kanounji@hotmail.com>
2026-04-11 12:45:25 +0200

    fix DB url, for alembic sqlalchemy

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -17,7 +17,7 @@ from db import models  # noqa: F401 — register models for autogenerate
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", settings.ASYNC_DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
